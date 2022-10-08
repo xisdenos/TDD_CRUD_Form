f83e54d533fad4a248a30e191c9ea04af8c3313f
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-10-08 17:41:57 -0300

    feature: SignUpFormModelValidator does not accept empty names anymore

--- TDD_CRUD-Form/TDD_CRUD-Form/SignupFormModelValidator.swift
+++ TDD_CRUD-Form/TDD_CRUD-Form/SignupFormModelValidator.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class SignupFormModelValidator {
+    
+    func isFirstNameNotEmpty(firstName: String) -> Bool {
+        var returnValue = true
+        
+        if firstName.isEmpty {
+            returnValue = false
+        }
+        
+        return returnValue
+    }
+}
