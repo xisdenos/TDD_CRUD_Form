eb1ef621f4beaa5f0a6280d6120abf83bd40616b
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-10-08 18:32:52 -0300

    test: refactoring of empty first name test to increase coverage

--- TDD_CRUD-Form/TDD_CRUD-FormTests/SignUpModelFormValidatorTest.swift
+++ TDD_CRUD-Form/TDD_CRUD-FormTests/SignUpModelFormValidatorTest.swift
@@ -15,38 +15,52 @@ import XCTest
 @testable import TDD_CRUD_Form
 
 final class SignUpModelFormValidator: XCTestCase {
+    
+    var sut: SignupFormModelValidator!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = SignupFormModelValidator()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
     func testSignUpModelFormValidator_WhenNotEmptyFirstNameProvided_ShouldReturnTrue() {
         
         // Arrange
-        let sut = SignupFormModelValidator()
-        
         // Act
-        let firstNameValid = sut.isFirstNameNotEmpty(firstName: "Lucas")
+        let firstNameValid = sut.isFirstNameNotEmpty(firstName: "s")
         
         // Assert
         switch firstNameValid {
         case .success(let result):
             XCTAssertTrue(result)
             
+        case .failure( _ ):
+            XCTFail("The isfirstNameValid() should have returned True for a valid first name but returned false")
+        }
+    }
+    
+    func testSignUpModelFormValidator_WhenEmptyFirstNameProvided_ShouldReturnEmptyError() {
+        
+        // Arrange
+        // Act
+        let firstNameValid = sut.isFirstNameNotEmpty(firstName: "")
+        
+        // Assert
+        switch firstNameValid {
+        case .success( _ ):
+            XCTFail("Test need the result to fail")
+            
         case .failure(let error):
-            XCTAssertEqual(error, FormModelValidatorError.emptyName, "The isfirstNameValid() should have returned True for a valid first name but returned false")
+            XCTAssertEqual(error, FormModelValidatorError.emptyName, "The isfirstNameValid() should have returned correct empty name error but returned \(error)")
         }
     }
     
     func testSignUpModelFormValidator_WhenFirstNameProvided_ShouldRespectCorrectLength() {
         
         // Arrange
-        let sut = SignupFormModelValidator()
-        
         // Act
         let correctLengthFirstName = sut.isFirstNameNotEmpty(firstName: "Lucas")
         
