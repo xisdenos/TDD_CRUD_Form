c1600f3335ce95cf09f812923addbb4574a09b44
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-10-09 01:49:46 -0300

    test: SignUpModelValidatorPasswordTest should not accept password outside 8 to 24 characters

--- TDD_CRUD-Form/TDD_CRUD-FormTests/SignUpModelValidatorTests/SignUpModelFormPasswordValidatorTest.swift
+++ TDD_CRUD-Form/TDD_CRUD-FormTests/SignUpModelValidatorTests/SignUpModelFormPasswordValidatorTest.swift
@@ -13,7 +13,7 @@ final class SignUpModelFormPasswordValidatorTest: XCTestCase {
     var sut: SignupFormModelValidator!
 
     override func setUpWithError() throws {
-        sut = SignupFormModelValidator(firstName: "generic_name", password: "1234")
+        sut = SignupFormModelValidator(firstName: "generic_name", password: "12345678")
     }
 
     override func tearDownWithError() throws {
@@ -35,7 +35,7 @@ final class SignUpModelFormPasswordValidatorTest: XCTestCase {
         }
     }
     
-    func testSignUpModelFormValidator_WhenEmptyFirstNameProvided_ShouldReturnEmptyError() {
+    func testSignUpModelFormValidator_WhenEmptyPasswordProvided_ShouldReturnEmptyError() {
         
         // Arrange
         // Act
@@ -48,6 +48,22 @@ final class SignUpModelFormPasswordValidatorTest: XCTestCase {
             
         case .failure(let error):
             XCTAssertEqual(error, FormModelValidatorPasswordError.emptyPassword, "The isPasswordNotEmpty() should have returned correct empty password error but returned \(error)")
+        }
+    }
+    
+    func testSignUpModelFormValidator_WhenPasswordProvided_ShouldRespectCorrectLength() {
+        
+        // Arrange
+        // Act
+        let passwordValid = sut.correctPasswordLength()
+        
+        // Assert
+        switch passwordValid {
+        case .success(let result):
+            XCTAssertTrue(result)
+            
+        case .failure( _ ):
+            XCTFail("The correctNameLength() should have returned TRUE for a first name with size beetween 8 and 24 but returned FALSE")
         }
     }
 }
