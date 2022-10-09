d90bc704b3a7de0c95853f205f31c196d0cbf3c2
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-10-09 01:20:47 -0300

    test: SignUpFormModelValidator should not accept empty password

--- TDD_CRUD-Form/TDD_CRUD-FormTests/SignUpModelValidatorTests/SignUpModelFormPasswordValidatorTest.swift
+++ TDD_CRUD-Form/TDD_CRUD-FormTests/SignUpModelValidatorTests/SignUpModelFormPasswordValidatorTest.swift
@@ -6,16 +6,48 @@
 //
 
 import XCTest
+@testable import TDD_CRUD_Form
 
 final class SignUpModelFormPasswordValidatorTest: XCTestCase {
+    
+    var sut: SignupFormModelValidator!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = SignupFormModelValidator(firstName: "generic_name", password: "1234")
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
+    func testSignUpModelFormValidator_WhenNotEmptyPasswordProvided_ShouldReturnTrue() {
+        
+        // Arrange
+        // Act
+        let passwordValid = sut.isPasswordNotEmpty()
+        // Assert
+        switch passwordValid {
+        case .success(let result):
+            XCTAssertTrue(result)
+            
+        case .failure( _ ):
+            XCTFail("The isPasswordNotEmpty() should have returned TRUE for a not empty first name but returned FALSE")
+        }
+    }
     
+    func testSignUpModelFormValidator_WhenEmptyFirstNameProvided_ShouldReturnEmptyError() {
+        
+        // Arrange
+        // Act
+        sut.password = ""
+        let passwordValid = sut.isPasswordNotEmpty()
+        // Assert
+        switch passwordValid {
+        case .success( _ ):
+            XCTFail("Test need the result to fail")
+            
+        case .failure(let error):
+            XCTAssertEqual(error, FormModelValidatorPasswordError.emptyPassword, "The isPasswordNotEmpty() should have returned correct empty password error but returned \(error)")
+        }
+    }
 }
